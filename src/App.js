a569f6f03b87540a227b31646ce2bc8687563ba1
Zaggle <zaggle@Zaggles-MacBook-Pro.local>
2024-04-30 11:31:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Header from './Header/header';
+import Footer from './Footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        <p>
+           <Header/><br></br>
+           <Footer/>
+        </p>
+         
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Footer/Footer.js
@@ -0,0 +1,24 @@
+
+
+import React from 'react'
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <>
+     <div className='Footer'>
+        <h1>
+         <ul>
+            <li>Abc</li>
+            <li>123</li>
+         </ul>
+         </h1>
+     </div>
+    
+    
+    
+    </>
+  )
+}
+
+export default Footer;
